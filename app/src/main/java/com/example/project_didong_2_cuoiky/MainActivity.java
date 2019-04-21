1867787c3bce5ea707322836ff1d66777cb1f0b6
xuantruong20011992 <33837274+xuantruong20011992@users.noreply.github.com>
2019-04-22 05:43:17 +0700

    Đồ án Đi Động 2 - Đào Đặng Xuân Trường

---
+++ app/src/main/java/com/example/project_didong_2_cuoiky/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.project_didong_2_cuoiky;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
